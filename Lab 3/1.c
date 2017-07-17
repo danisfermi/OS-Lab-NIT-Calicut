cba00838387d01541f106ea2debe50c101d65395
Danis Fermi <danisfermijohn@gmail.com>
2017-07-18 00:06:10 +0530

    Added Lab 3 Solutions Q1 and Q2

---
+++ Lab 3/1.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int var_glb; // A global variable
+
+int main(void)
+{
+    pid_t childPID;
+    int var_lcl = 0;
+
+    childPID = fork();
+
+    if(childPID >= 0) // Fork was successful
+    {
+        if(childPID == 0) // Child process
+        {
+            var_lcl++;
+            var_glb++;
+            printf("\n Child Process :: var_lcl = [%d], var_glb[%d]\n", var_lcl, var_glb);
+        }
+        else // Parent process
+        {
+            var_lcl = 10;
+            var_glb = 20;
+            printf("\n Parent process :: var_lcl = [%d], var_glb[%d]\n", var_lcl, var_glb);
+        }
+    }
+    else // Fork failed
+    {
+        printf("\n Fork failed.\n");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ Lab 3/2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+int main(int argc, char **argv)
+{
+  char buffer[1024];
+  int files[2];
+  size_t count;
+  if (argc<3) // Checking if we have sufficient arguments
+    return -1;
+  files[0] = open(argv[1], O_RDONLY);
+  if (files[0] == -1) // Checking if the file opened
+    return -1;
+  files[1] = open(argv[2], O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR);
+  if (files[1] == -1) // Checking if 2nd file opened
+  {
+    close(files[0]);
+    return -1;
+  }
+  while ((count = read(files[0], buffer, sizeof(buffer))) != 0)
+    write(files[1], buffer, count);
+
+  return 0;
+
+}
